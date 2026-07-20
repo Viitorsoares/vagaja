7af69aef9dc40e4d27b2d06ed5aba24edf6f8d14
Viitorsoares <viitorgab.soares@gmail.com>
2026-07-20 19:04:03 -0300

    Criando componente mapa

---
+++ src/components/map.tsx
@@ -0,0 +1,10 @@
+import { useEffect, useRef } from "react";
+
+export default function Map() {
+    const mapRef = useRef()
+    const mapContainerRef = useRef()
+
+    useEffect(() => {
+        
+    })
+}
